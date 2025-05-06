92afcf0267b04ad1e44b66314359b1da8f820612
Joelma Lima Printes <joelmabl78@gmail.com>
2025-05-06 16:50:52 -0400

    Add files via upload

---
+++ concatenar.py
@@ -0,0 +1,10 @@
+num1 = (1, 2, 2, 3)
+num2 = (4, 5, 6)
+
+# Juntando as tuplas
+todosnum = num1 + num2
+print(f"Resultado das tuplas ordenadas: {todosnum}")
+
+# Contando quantas vezes o número 2 aparece em num1
+contar = num1.count(2)
+print(f"O número 2 se repete: {contar} vezes")
